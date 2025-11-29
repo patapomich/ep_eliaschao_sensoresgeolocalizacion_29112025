28ffc8928b31916fc2fd7194757f1d0135583ff3
patapon-glitch <eliaschao94@gmail.com>
2025-11-29 15:40:30 -0400

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ep_eliaschao_sensoresgeolocalizacion_29112025"
+include(":app")
+ 
